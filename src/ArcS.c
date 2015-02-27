5e63ea0277ff18a7443c840508e63db439e30c19
Christian M. Schmid <chms@gmx.net>
2015-02-27 22:17:51 +0100

    Almost initial commit

---
+++ src/ArcS.c
@@ -0,0 +1,45 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+  // Create main Window element and assign to pointer
+  window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(window, true);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
