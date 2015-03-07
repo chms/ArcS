7ddc9001b31084029022409d9fb1e7de57289f2d
Christian M. Schmid <chms@gmx.net>
2015-03-08 00:58:29 +0100

    Added battery and bluetooth indicators

--- src/ArcS.c
+++ src/ArcS.c
@@ -18,6 +18,14 @@
 
 #include "pebble.h"
 
+// Compile options
+#define DIGITAL_CLOCK_DURATION 5000
+#define DIGITAL_ALWAYS_ON      true
+#define FAKE_TIME              false
+#define ARCS_PLUS              true
+#define ARCS_CS                false
+
+// Dimensions
 #define MINUTE_CIRCLE_THICKNESS  4
 #define HOUR_CIRCLE_THICKNESS    8
 #define CIRCLE_SPACING_THICKNESS 6
@@ -25,9 +33,9 @@
 #define LOGO_WIDTH   78
 #define LOGO_HEIGHT  48
 
-#define DIGITAL_CLOCK_DURATION 5000
-#define HIDE_LOGO              true
-#define DIGITAL_ALWAYS_ON      true
+// Color schemes
+#define COLOR_SCHEME_APLITE_DARK
+#define COLOR_SCHEME_APLITE_LIGHT
 
 static Window *window;
 static GBitmap *bitmap_logo;
@@ -45,6 +53,8 @@ static bool digital_clock_is_visible;
 static int angle_90 = TRIG_MAX_ANGLE / 4;
 static int angle_180 = TRIG_MAX_ANGLE / 2;
 static int angle_270 = 3 * TRIG_MAX_ANGLE / 4;
+
+static GColor Color0, Color1;
 
 static void graphics_draw_arc(GContext *ctx, GPoint center, int radius, int thickness, int start_angle, int end_angle, GColor c) {
   int32_t xmin = 65535000, xmax = -65535000, ymin = 65535000, ymax = -65535000;
@@ -160,22 +170,26 @@ static void graphics_draw_arc(GContext *ctx, GPoint center, int radius, int thic
   }
 }
 
-static void update_circles(Layer *layer, GContext *ctx) {
+static void update_layer_arcs(Layer *layer, GContext *ctx) {
+    #if FAKE_TIME
+    time_t now = 419903100;
+    #else
     time_t now = time(NULL);
+    #endif
     struct tm *tick_time = localtime(&now);
     int32_t hour_angle   = TRIG_MAX_ANGLE * (tick_time->tm_hour%12)/12 + TRIG_MAX_ANGLE/12 * tick_time->tm_min/60;
     int32_t minute_angle = TRIG_MAX_ANGLE * tick_time->tm_min/60;
     //int32_t hour_angle   = TRIG_MAX_ANGLE-1;
     //int32_t minute_angle = TRIG_MAX_ANGLE-1;
-    
-    graphics_draw_arc(ctx, window_center, window_center.x, HOUR_CIRCLE_THICKNESS, angle_270, hour_angle+angle_270, GColorWhite);
-    graphics_draw_arc(ctx, window_center, window_center.x-CIRCLE_SPACING_THICKNESS-HOUR_CIRCLE_THICKNESS, MINUTE_CIRCLE_THICKNESS, angle_270, minute_angle+angle_270, GColorWhite);
+
+    graphics_draw_arc(ctx, window_center, window_center.x, HOUR_CIRCLE_THICKNESS, angle_270, hour_angle+angle_270, Color1);
+    graphics_draw_arc(ctx, window_center, window_center.x-CIRCLE_SPACING_THICKNESS-HOUR_CIRCLE_THICKNESS, MINUTE_CIRCLE_THICKNESS, angle_270, minute_angle+angle_270, Color1);
 }
 
-static void update_battery_bluetooth(Layer *layer, GContext *ctx) {
+static void update_layer_battery_bluetooth(Layer *layer, GContext *ctx) {
     uint8_t shift_battery = 6;
 
-    graphics_context_set_stroke_color(ctx, GColorWhite);
+    graphics_context_set_stroke_color(ctx, Color1);
     if (bluetooth_connection_service_peek()) {
         graphics_draw_line(ctx, GPoint(24,0), GPoint(24,12));
         graphics_draw_line(ctx, GPoint(21,3), GPoint(27,9));
@@ -189,21 +203,19 @@ static void update_battery_bluetooth(Layer *layer, GContext *ctx) {
     graphics_draw_line(ctx, GPoint(14+shift_battery,5), GPoint(14+shift_battery,8));
     
     BatteryChargeState battery_state = battery_state_service_peek();
-    uint8_t charge_percent = battery_state.charge_percent + 10;
+    uint8_t charge_percent = battery_state.charge_percent;
     uint8_t x_bar = shift_battery + 2;
     
-
     // Fill battery bars
-    while(charge_percent > 15) {
+    while(charge_percent > 10) {
         graphics_draw_line(ctx, GPoint(x_bar,5), GPoint(x_bar, 8));
         x_bar++;
-        charge_percent = charge_percent - 10;
+        charge_percent = charge_percent - 9;
     }
-    
 }
 
 static void update_digital_clock_t(struct tm *tick_time) {
-    static char s_time_text[] = "20:00";
+    static char s_time_text[] = "00:00";
     static char s_date_text[] = "Xxx, 00.00.";
     char *time_format;
 
@@ -218,19 +230,23 @@ static void update_digital_clock_t(struct tm *tick_time) {
 }
 
 static void update_digital_clock() {
+    #if FAKE_TIME
+    time_t now = 419903100;
+    #else
     time_t now = time(NULL);
+    #endif
     struct tm *tick_time = localtime(&now);
     update_digital_clock_t(tick_time);
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-    update_digital_clock_t(tick_time);
+    update_digital_clock();
     layer_mark_dirty(layer_arcs);
 }
 
 static void show_digital_clock(bool show_digital_clock) {
     digital_clock_is_visible = show_digital_clock || DIGITAL_ALWAYS_ON;
-    layer_set_hidden(bitmap_layer_get_layer(bitmap_layer_logo), digital_clock_is_visible || HIDE_LOGO);
+    layer_set_hidden(bitmap_layer_get_layer(bitmap_layer_logo), digital_clock_is_visible || !ARCS_CS);
     layer_set_hidden(layer_battery_bluetooth, !digital_clock_is_visible);
     layer_set_hidden(text_layer_get_layer(text_layer_time), !digital_clock_is_visible);
     layer_set_hidden(text_layer_get_layer(text_layer_date), !digital_clock_is_visible);
@@ -245,9 +261,17 @@ static void tap_handler(AccelAxisType axis, int32_t direction) {
     show_digital_clock(true);
 }
 
+static void battery_handler(BatteryChargeState charge) {
+    layer_mark_dirty(layer_battery_bluetooth);
+}
+
+static void bluetooth_handler(bool connected) {
+    layer_mark_dirty(layer_battery_bluetooth);
+}
+
 static void window_load(Window *window) {
     // Set window background to black and get window layer and dimensions
-    window_set_background_color(window, GColorBlack);
+    window_set_background_color(window, Color0);
     Layer *window_layer = window_get_root_layer(window);
     window_bounds   = layer_get_bounds(window_layer);
     window_center.x = window_bounds.size.w/2;
@@ -263,22 +287,22 @@ static void window_load(Window *window) {
     text_layer_set_font(text_layer_time, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
     text_layer_set_text_alignment(text_layer_time, GTextAlignmentCenter);
     text_layer_set_background_color(text_layer_time, GColorClear);
-    text_layer_set_text_color(text_layer_time, GColorWhite);
+    text_layer_set_text_color(text_layer_time, Color1);
 
     // Create and style digital date layer
     text_layer_date = text_layer_create((GRect) { .origin = { 0, window_center.y+9}, .size = { window_bounds.size.w, 34 } });
     text_layer_set_font(text_layer_date, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text_alignment(text_layer_date, GTextAlignmentCenter);
     text_layer_set_background_color(text_layer_date, GColorClear);
-    text_layer_set_text_color(text_layer_date, GColorWhite);
+    text_layer_set_text_color(text_layer_date, Color1);
 
     // Create and style arc (analog) layer
     layer_arcs = layer_create(window_bounds);
-    layer_set_update_proc(layer_arcs, update_circles);
+    layer_set_update_proc(layer_arcs, update_layer_arcs);
 
     // Create and style battery layer
     layer_battery_bluetooth = layer_create(GRect(58, 44, 28, 13));
-    //layer_set_update_proc(layer_battery_bluetooth, update_battery_bluetooth);
+    layer_set_update_proc(layer_battery_bluetooth, update_layer_battery_bluetooth);
     
     // Update time and date
     update_digital_clock();
@@ -289,46 +313,65 @@ static void window_load(Window *window) {
     layer_add_child(window_layer, text_layer_get_layer(text_layer_time));
     layer_add_child(window_layer, text_layer_get_layer(text_layer_date));
     layer_add_child(window_layer, layer_arcs);
+    
+    #if ARCS_PLUS
     layer_add_child(window_layer, layer_battery_bluetooth);
+    #endif
 }
 
 static void window_unload(Window *window) {
-  bitmap_layer_destroy(bitmap_layer_logo);
-  gbitmap_destroy(bitmap_logo);
-  text_layer_destroy(text_layer_time);
-  text_layer_destroy(text_layer_date);
-  layer_destroy(layer_arcs);
-  layer_destroy(layer_battery_bluetooth);
+    bitmap_layer_destroy(bitmap_layer_logo);
+    gbitmap_destroy(bitmap_logo);
+    text_layer_destroy(text_layer_time);
+    text_layer_destroy(text_layer_date);
+    layer_destroy(layer_arcs);
+    layer_destroy(layer_battery_bluetooth);
+}
+
+static void load_color_scheme() {
+    Color0 = GColorBlack;
+    Color1 = GColorWhite;
 }
 
 static void init(void) {
-  // Create main window element and assign to pointer
-  window = window_create();
+    load_color_scheme();
 
-  // Set handlers to manage the elements inside the window
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
+    // Create main window element and assign to pointer
+    window = window_create();
 
-  // Show the window on the watch, with animated=true
-  window_stack_push(window, true);
+    // Set handlers to manage the elements inside the window
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+    });
+
+    // Show the window on the watch, with animated=true
+    window_stack_push(window, true);
   
-  // Add timer for minutes
-  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+    // Add timer for minutes
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 
-  // React to taps
-  accel_tap_service_subscribe(tap_handler);
+    // React to taps
+    accel_tap_service_subscribe(tap_handler);
+
+    #if ARCS_PLUS
+    // Listen to battery changes
+    battery_state_service_subscribe(battery_handler);
+    // Listen to bluetooth changes
+    bluetooth_connection_service_subscribe(bluetooth_handler);
+    #endif
 }
 
 static void deinit(void) {
-  tick_timer_service_unsubscribe();
-  accel_tap_service_unsubscribe();
-  window_destroy(window);
+    tick_timer_service_unsubscribe();
+    accel_tap_service_unsubscribe();
+    battery_state_service_unsubscribe();
+    bluetooth_connection_service_unsubscribe();
+    window_destroy(window);
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
-}
+    init();
+    app_event_loop();
+    deinit();
+}  
